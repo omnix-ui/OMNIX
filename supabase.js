35c29f6a7e4e292171ce486e0ffb58e61f3f92f8
OMNIX <omnix500@gmail.com>
2026-07-10 22:17:07 +0530

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase.js - Sirf Database ka setup
+
+const supabaseUrl = 'otbyobzvuomysphfgmym';
+const supabaseKey = 'sb_publishable_ETKnkiMrAU-1DBSVAwQ2RA_hqvpLZLE';
+
+// Supabase client ko global variable bana rahe hain taaki baaki files isko use kar sakein
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase alag file se connect ho gaya hai:", supabase);
