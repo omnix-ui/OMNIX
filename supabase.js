c95b2c6e0c313d0126b5d033f9faf84811c0f188
OMNIX <omnix500@gmail.com>
2026-07-10 23:56:18 +0530

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js - Sirf Database ka setup
 
-const supabaseUrl = 'otbyobzvuomysphfgmym';
-const supabaseKey = 'sb_publishable_ETKnkiMrAU-1DBSVAwQ2RA_hqvpLZLE';
+const supabaseUrl = 'https://otbyobzvuomysphfgmym.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90YnlvYnp2dW9teXNwaGZnbXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2Nzk5MTMsImV4cCI6MjA5OTI1NTkxM30.UKLCLo3TrlTNAVv2-dl_m8w8YdudALZO05_R5iEKd64';
 
 // Supabase client ko global variable bana rahe hain taaki baaki files isko use kar sakein
 window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
