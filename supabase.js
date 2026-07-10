9b4d7696b529a59eca45292f8f4715031abc211d
OMNIX <omnix500@gmail.com>
2026-07-10 23:13:22 +0530

    supabase.js

--- supabase.js
+++ supabase.js
@@ -4,6 +4,6 @@ const supabaseUrl = 'otbyobzvuomysphfgmym';
 const supabaseKey = 'sb_publishable_ETKnkiMrAU-1DBSVAwQ2RA_hqvpLZLE';
 
 // Supabase client ko global variable bana rahe hain taaki baaki files isko use kar sakein
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-console.log("Supabase alag file se connect ho gaya hai:", supabase);
+console.log("Supabase alag file se connect ho gaya hai:", supabaseClient);
